4b9e034828124df6e0abb47b88447e37eac0bace
Justin Hunt <poodllsupport@gmail.com>
2015-03-02 17:15:48 +0900

    version bump

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015011500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2015030100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2013110500;        // Requires this Moodle version.
 $plugin->component = 'atto_generico';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
