cecd04e7ea484836599ecb68a2ae9a96f52e64ba
Justin Hunt <poodllsupport@gmail.com>
2016-06-12 12:24:02 +0000

    bumped version

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016030900;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016061200;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2013110500;        // Requires this Moodle version.
 $plugin->component = 'atto_generico';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
 // Human readable version informatiomn
-$plugin->release   = '1.0.7 (Build 2016030900)';
+$plugin->release   = '1.0.8 (Build 2016061200)';
 $plugin->dependencies = array('filter_generico' => 2016011800);
 
