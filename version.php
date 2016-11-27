75cb713ab86e786ce9632fa9a4dcc99f08283f0f
Justin Hunt <poodllsupport@gmail.com>
2016-11-27 07:56:52 -0500

    fixed up styling for Moodle 3.2 boost theme

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016061200;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016112701;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2013110500;        // Requires this Moodle version.
 $plugin->component = 'atto_generico';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
 // Human readable version informatiomn
-$plugin->release   = '1.0.8 (Build 2016061200)';
+$plugin->release   = '1.0.9 (Build 2016112701)';
 $plugin->dependencies = array('filter_generico' => 2016011800);
 
